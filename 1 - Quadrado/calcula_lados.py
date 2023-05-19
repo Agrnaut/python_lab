340f10415f24c007e3ec98ea885e6a78cf9b2011
Agrnaut <54293929+Agrnaut@users.noreply.github.com>
2023-05-19 18:37:05 -0300

    Update calcula_lados.py
    Adjusting

--- 1 - Quadrado/calcula_lados.py
+++ 1 - Quadrado/calcula_lados.py
@@ -1,10 +1,13 @@
 from Quadrado import Quadrado
 
-def quer_continuar(valor):
+def quer_continuar():
+    valor = ((int(input("Gostaria de voltar ao menu anterior?\n [1] Sim\n [2] Não\n"))))
     if (valor == 1):
         return False
     else:
         return True
+
+
 
 print("Bem vindo Aluno")
 
@@ -14,18 +17,29 @@ quadrado_exercicio = Quadrado(float(input("Digite um tamanho para os lados do se
 
 
 fim_de_loop = False
-
 while(not fim_de_loop):
+
     pergunta = int(input(("Você gostaria de "
-          "\n[1] Mostrar o tamanho do lado e o calculo da área: "
-          "\n[2] Mudar o tamanho do lado do seu quadrado:\n: ")))
-    if (pergunta < 0 or pergunta > 2):
-        print("opção inválida! Escolha outra\n")
-        continue
-    elif(pergunta == 1):
-        quadrado_exercicio.mostra_e_calcula
-        fim_de_loop = quer_continuar((int(input("Gostaria de voltar ao menu anterior?\n [1] Sim\n [2] Não\n"))))
-    else:
-        quadrado_exercicio.muda_tamanho_lado(float(input("Digite o novo lado do seu quadrado: ")))
-        continue
+          "\n[1] Mostrar o tamanho dos lados "
+          "\n[2] Calcular a área do seu quadrado"
+          "\n[3] Mudar o tamanho do lado do seu quadrado"
+                          "\nOpção: ")))
+
+    # fim_de_loop = quer_continuar()
+
+    for escolha in range(3):
+        if(pergunta == 1):
+            print(f"O Lado do seu quadrado é {quadrado_exercicio.tamanho}")
+            fim_de_loop = quer_continuar()
+            break
+        elif(pergunta == 2):
+            print(quadrado_exercicio.calculo())
+            fim_de_loop = quer_continuar()
+            break
+        elif(pergunta == 3):
+            quadrado_exercicio.novo_tamanho(float(input("Digite um novo tamanho para os lados do seu quadrado: ")))
+            fim_de_loop = quer_continuar()
+            break
+        else:
+            print("Opção inválida")
 
