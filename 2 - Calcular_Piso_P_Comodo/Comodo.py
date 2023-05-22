7d5c19505ee55a7c79bc8d9e776dc21101775a0c
Jose_B_Ramos <54293929+Agrnaut@users.noreply.github.com>
2023-05-22 14:36:17 -0300

    Delete 2 - Calcular_Piso_P_Comodo directory

--- 2 - Calcular_Piso_P_Comodo/Comodo.py
+++
@@ -1,8 +0,0 @@
-class Comodo:
-    def __init__(self, comprimento, largura):
-        self._comprimento = comprimento
-        self._largura = largura
-
-
-    def calcula_m2(self):
-        return (float(self._comprimento * self._largura))
